44c98c1aea78304b008df40123788439619c974b
Weinan Li <liweinan0423@icloud.com>
2024-09-22 18:22:19 +0800

    test

--- lib/vars/pipelines/commitPipeline.groovy
+++ lib/vars/pipelines/commitPipeline.groovy
@@ -1,11 +1,12 @@
-import jobs.example1;
+import jobs.Example1;
 
+example1 = new Example1();
 def call() {
 	pipeline {
 		agent any
 		node {
 			stage "hello" {
-				example1
+				example1.call();
 			}
 		}
 	}
