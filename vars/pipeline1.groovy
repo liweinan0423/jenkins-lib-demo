d1e7b7df5c96f7cda3891262a6444a3959253531
Weinan Li <liweinan0423@icloud.com>
2024-09-23 09:25:36 +0800

    test

--- vars/pipeline1.groovy
+++ vars/pipeline1.groovy
@@ -1,20 +1,11 @@
 def call() {
-    def pipeline = new org.mycompany.pipeline.Pipeline()
-
-    pipeline.addStage(new org.mycompany.pipeline.Stage('Build', {
-        // Define build steps
-        println 'Building...'
-    }))
-
-    pipeline.addStage(new org.mycompany.pipeline.Stage('Test', {
-        // Define test steps
-        println 'Testing...'
-    }))
-
-    pipeline.addStage(new org.mycompany.pipeline.Stage('Deploy', {
-        // Define deploy steps
-        println 'Deploying...'
-    }))
-
-    pipeline.execute()
+    pipeline {
+        stage('hello') {
+            script {
+                sh'''
+                echo Hello
+                '''
+            }
+        }
+    }
 }
